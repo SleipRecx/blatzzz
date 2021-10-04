9b8234f683e1c2e590ca9c09f4d037f88afefe0f
Markus Andresen <markus.andresen@eika.no>
2021-10-04 19:22:21 +0200

    Design

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
 import React, { useEffect, useRef, useState } from "react";
 import { getDatabase, ref as dbRef, set, onValue } from "firebase/database";
 import { getStorage, ref, uploadBytes, getDownloadURL } from "firebase/storage";
+import {
+  Box,
+  Card,
+  CardContent,
+  CardMedia,
+  CircularProgress,
+} from "@mui/material";
 
 function UploadImage() {
+  const [loading, setLoading] = useState(false);
   const [file, setFile] = useState(null);
   const fileRef = useRef(null);
 
@@ -10,7 +18,7 @@ function UploadImage() {
   const storage = getStorage();
 
   return (
-    <>
+    <div style={{ padding: 50 }}>
       <input
         ref={fileRef}
         type="file"
@@ -24,28 +32,44 @@ function UploadImage() {
       <button
         type="submit"
         onClick={async () => {
-          console.log(file);
+          setLoading(true);
           const imageRef = ref(storage, "images/" + file.name);
           await uploadBytes(imageRef, file);
           const url = await getDownloadURL(imageRef);
-          const result = await set(dbRef(db, "posts/" + Date.now()), {
+          await set(dbRef(db, "posts/" + Date.now()), {
             url,
           });
-          console.log(result);
           fileRef.current.value = "";
+          setLoading(false);
         }}
       >
         Submit
       </button>
-    </>
+      <br />
+      <br />
+      {loading && (
+        <Box sx={{ display: "flex" }}>
+          <CircularProgress />
+        </Box>
+      )}
+    </div>
   );
 }
 
 function Post({ uuid, url }) {
   return (
-    <div key={uuid}>
-      <img alt="user uploaded" src={url} />
-      <p>{new Date(parseInt(uuid)).toLocaleString()}</p>
+    <div key={uuid} style={{ padding: 50 }}>
+      <Card sx={{ maxWidth: 345 }}>
+        <CardMedia
+          component="img"
+          height="350"
+          image={url}
+          alt="User uploaded"
+        />
+        <CardContent>
+          <p>{new Date(parseInt(uuid)).toLocaleString()}</p>
+        </CardContent>
+      </Card>
     </div>
   );
 }
@@ -56,17 +80,22 @@ function Feed() {
   useEffect(() => {
     const databaseRef = dbRef(db, "posts/");
     return onValue(databaseRef, (snapshot) => {
-      const data = Object.entries(snapshot.val()).map(([key, value]) => {
-        return { id: key, url: value.url };
-      });
+      const data = Object.entries(snapshot.val())
+        .map(([key, value]) => {
+          return { id: key, url: value.url };
+        })
+        .sort((a, b) => parseInt(b.id) - parseInt(a.id));
       setPosts(data);
     });
   }, [db]);
 
-  const rawPosts = posts.map((post) => (
-    <Post key={post.id} uuid={post.id} url={post.url} />
-  ));
-  return rawPosts;
+  return (
+    <div style={{ display: "flex", flexDirection: "column" }}>
+      {posts.map((post) => (
+        <Post key={post.id} uuid={post.id} url={post.url} />
+      ))}
+    </div>
+  );
 }
 
 function App() {
