92e2d6170e6c2bf84089abf7ac0a8099911f08d7
Markus Andresen <markus.andresen@eika.no>
2021-10-06 10:52:00 +0200

    Change hosting

--- src/firebase.js
+++ src/firebase.js
@@ -7,14 +7,15 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyCGxezDHhLWb7bNfJ-bKmEaTzMwnA7mo_E",
-    authDomain: "hyre-demo-ca592.firebaseapp.com",
-    databaseURL: "https://hyre-demo-ca592-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "hyre-demo-ca592",
-    storageBucket: "hyre-demo-ca592.appspot.com",
-    messagingSenderId: "430857610465",
-    appId: "1:430857610465:web:a534bbc8beeef14b67c3a0",
-    measurementId: "G-JBRWV0187Q"
+  apiKey: "AIzaSyCGxezDHhLWb7bNfJ-bKmEaTzMwnA7mo_E",
+  authDomain: "blatzzz.firebaseapp.com",
+  databaseURL:
+    "https://hyre-demo-ca592-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "hyre-demo-ca592",
+  storageBucket: "hyre-demo-ca592.appspot.com",
+  messagingSenderId: "430857610465",
+  appId: "1:430857610465:web:a534bbc8beeef14b67c3a0",
+  measurementId: "G-JBRWV0187Q",
 };
 
 // Initialize Firebase
